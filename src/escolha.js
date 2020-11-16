fd5f1f59329bf2588c43fc022a2986877e51136f
marceloll <marcelo.lopes.lotufo@gmail.com>
2020-11-16 14:08:11 -0300

    Adicione exemplo de escolha

---
+++ src/escolha.js
@@ -0,0 +1,21 @@
+var número = Math.floor(Math.random() * 10);
+
+escolha (número) {
+  caso 0:
+    console.log("Foi 0");
+    pare;
+  caso 1:
+    console.log("Foi 0");
+    pare;
+  caso 2:
+    console.log("Foi 0");
+    pare;
+  caso 3:
+    console.log("Foi 0");
+    pare;
+  caso 4:
+    console.log("Foi 0");
+    pare;
+  padrão:
+    console.log("Foi maior que 4");
+}
