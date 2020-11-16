c0df50a9a91cfe7cee8151ad2a5b7c8c6c93c6d1
marceloll <marcelo.lopes.lotufo@gmail.com>
2020-11-16 12:06:15 -0300

    Crie exemplo de condicional

---
+++ src/condicional.js
@@ -0,0 +1,13 @@
+se (verdadeiro) {
+  console.log("Era verdadeiro");
+}
+
+se (!falso) {
+  console.log("Era falso");
+}
+
+se (verdadeiro && falso) {
+  console.log("não serei chamado");
+} senão {
+  console.log("serei chamado");
+}
